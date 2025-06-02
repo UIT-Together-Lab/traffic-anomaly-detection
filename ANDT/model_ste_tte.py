2ed2e13e96846b3afcd571e3833a5dead6612740
UIT-Together-Lab <uit.together@gmail.com>
2025-06-02 15:01:00 +0700

    ANDT

---
+++ ANDT/model_ste_tte.py
@@ -0,0 +1,379 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import pdb
+import torchvision
+from torchvision.models._utils import IntermediateLayerGetter
+import torch.distributed as dist
+import timm
+
+def is_dist_avail_and_initialized():
+    if not dist.is_available():
+        return False
+    if not dist.is_initialized():
+        return False
+    return True
+
+def get_world_size():
+    if not is_dist_avail_and_initialized():
+        return 1
+    return dist.get_world_size()
+
+def get_rank():
+    if not is_dist_avail_and_initialized():
+        return 0
+    return dist.get_rank()
+
+def is_main_process():
+    return get_rank() == 0
+
+class CrossAttention(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        # NOTE scale factor was wrong in my original version, can set manually to be compat with prev weights
+        self.scale = qk_scale or head_dim ** -0.5
+
+        self.wq = nn.Linear(dim, dim, bias=qkv_bias)
+        self.wk = nn.Linear(dim, dim, bias=qkv_bias)
+        self.wv = nn.Linear(dim, dim, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x):
+
+        B, N, C = x.shape
+        q = self.wq(x[:, 0:1, ...]).reshape(B, 1, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)  # B1C -> B1H(C/H) -> BH1(C/H)
+        k = self.wk(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)  # BNC -> BNH(C/H) -> BHN(C/H)
+        v = self.wv(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)  # BNC -> BNH(C/H) -> BHN(C/H)
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale  # BH1(C/H) @ BH(C/H)N -> BH1N
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, 1, C)   # (BH1N @ BHN(C/H)) -> BH1(C/H) -> B1H(C/H) -> B1C
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+class PositionEmbs(nn.Module):
+    def __init__(self, num_patches, emb_dim, dropout_rate=0.1):
+        super(PositionEmbs, self).__init__()
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, emb_dim))
+        if dropout_rate > 0:
+            self.dropout = nn.Dropout(dropout_rate)
+        else:
+            self.dropout = None
+
+    def forward(self, x):
+        out = x + self.pos_embedding
+
+        if self.dropout:
+            out = self.dropout(out)
+
+        return out
+
+class MlpBlock(nn.Module):
+    """ Transformer Feed-Forward Block """
+    def __init__(self, in_dim, mlp_dim, out_dim, dropout_rate=0.1):
+        super(MlpBlock, self).__init__()
+
+        # init layers
+        self.fc1 = nn.Linear(in_dim, mlp_dim)
+        self.fc2 = nn.Linear(mlp_dim, out_dim)
+        self.act = nn.GELU()
+        if dropout_rate > 0.0:
+            self.dropout1 = nn.Dropout(dropout_rate)
+            self.dropout2 = nn.Dropout(dropout_rate)
+        else:
+            self.dropout1 = None
+            self.dropout2 = None
+
+    def forward(self, x):
+
+        out = self.fc1(x)
+        out = self.act(out)
+        if self.dropout1:
+            out = self.dropout1(out)
+
+        out = self.fc2(out)
+        out = self.dropout2(out)
+        return out
+
+
+class LinearGeneral(nn.Module):
+    def __init__(self, in_dim=(768,), feat_dim=(12, 64)):
+        super(LinearGeneral, self).__init__()
+
+        self.weight = nn.Parameter(torch.randn(*in_dim, *feat_dim))
+        self.bias = nn.Parameter(torch.zeros(*feat_dim))
+
+    def forward(self, x, dims):
+        a = torch.tensordot(x, self.weight, dims=dims) + self.bias
+        return a
+
+
+class SelfAttention(nn.Module):
+    def __init__(self, in_dim, heads=8, dropout_rate=0.1):
+        super(SelfAttention, self).__init__()
+        self.heads = heads
+        self.head_dim = in_dim // heads
+        self.scale = self.head_dim ** 0.5
+
+        self.query = LinearGeneral((in_dim,), (self.heads, self.head_dim))
+        self.key = LinearGeneral((in_dim,), (self.heads, self.head_dim))
+        self.value = LinearGeneral((in_dim,), (self.heads, self.head_dim))
+        self.out = LinearGeneral((self.heads, self.head_dim), (in_dim,))
+
+        if dropout_rate > 0:
+            self.dropout = nn.Dropout(dropout_rate)
+        else:
+            self.dropout = None
+
+    def forward(self, x):
+        b, n, _ = x.shape
+
+        q = self.query(x, dims=([2], [0]))
+        k = self.key(x, dims=([2], [0]))
+        v = self.value(x, dims=([2], [0]))
+
+        q = q.permute(0, 2, 1, 3)
+        k = k.permute(0, 2, 1, 3)
+        v = v.permute(0, 2, 1, 3)
+
+        attn_weights = torch.matmul(q, k.transpose(-2, -1)) / self.scale
+        attn_weights = F.softmax(attn_weights, dim=-1)
+        out = torch.matmul(attn_weights, v)
+        out = out.permute(0, 2, 1, 3)
+
+        out = self.out(out, dims=([2, 3], [0, 1]))
+
+        return out
+
+
+class EncoderBlock(nn.Module):
+    def __init__(self, in_dim, mlp_dim, num_heads, dropout_rate=0.1, attn_dropout_rate=0.1):
+        super(EncoderBlock, self).__init__()
+
+        self.norm1 = nn.LayerNorm(in_dim)
+        self.attn = SelfAttention(in_dim, heads=num_heads, dropout_rate=attn_dropout_rate)
+        if dropout_rate > 0:
+            self.dropout = nn.Dropout(dropout_rate)
+        else:
+            self.dropout = None
+        self.norm2 = nn.LayerNorm(in_dim)
+        self.mlp = MlpBlock(in_dim, mlp_dim, in_dim, dropout_rate)
+
+    def forward(self, x):
+        residual = x
+        out = self.norm1(x)
+        out = self.attn(out)
+        if self.dropout:
+            out = self.dropout(out)
+        out += residual
+        residual = out
+
+        out = self.norm2(out)
+        out = self.mlp(out)
+        out += residual
+        return out
+
+
+class Encoder(nn.Module):
+    def __init__(self, num_patches, emb_dim, mlp_dim, num_layers=12, num_heads=12, dropout_rate=0.1, attn_dropout_rate=0.0):
+        super(Encoder, self).__init__()
+
+        # positional embedding
+        self.pos_embedding = PositionEmbs(num_patches, emb_dim, dropout_rate)
+
+        # encoder blocks
+        in_dim = emb_dim
+        self.encoder_layers = nn.ModuleList()
+        for i in range(num_layers):
+            layer = EncoderBlock(in_dim, mlp_dim, num_heads, dropout_rate, attn_dropout_rate)
+            self.encoder_layers.append(layer)
+        self.norm = nn.LayerNorm(in_dim)
+
+    def forward(self, x):
+        out = self.pos_embedding(x)
+        
+        for layer in self.encoder_layers:
+            out = layer(out)
+        
+        out = self.norm(out)
+        return out
+
+
+class Decoder(nn.Module):
+    def __init__(self, z_dim=512):
+        super(Decoder, self).__init__()
+        self.z_dim = z_dim
+
+        def Basic(intInput, intOutput):
+            return torch.nn.Sequential(
+                torch.nn.Conv2d(in_channels=intInput, out_channels=intOutput, kernel_size=3, stride=1, padding=1),
+                torch.nn.BatchNorm2d(intOutput),
+                torch.nn.ReLU(inplace=False),
+                torch.nn.Conv2d(in_channels=intOutput, out_channels=intOutput, kernel_size=3, stride=1, padding=1),
+                torch.nn.BatchNorm2d(intOutput),
+                torch.nn.ReLU(inplace=False)
+            )
+
+        def Gen(intInput, intOutput, nc):
+            return torch.nn.Sequential(
+                torch.nn.Conv2d(in_channels=intInput, out_channels=nc, kernel_size=3, stride=1, padding=1),
+                torch.nn.BatchNorm2d(nc),
+                torch.nn.ReLU(inplace=False),
+                torch.nn.Conv2d(in_channels=nc, out_channels=nc, kernel_size=3, stride=1, padding=1),
+                torch.nn.BatchNorm2d(nc),
+                torch.nn.ReLU(inplace=False),
+                torch.nn.Conv2d(in_channels=nc, out_channels=intOutput, kernel_size=3, stride=1, padding=1),
+                torch.nn.Tanh()
+            )
+
+        def Upsample(nc, intOutput):
+            return torch.nn.Sequential(
+                torch.nn.ConvTranspose2d(in_channels=nc, out_channels=intOutput, kernel_size=3, stride=2, padding=1,
+                                         output_padding=1),
+                torch.nn.BatchNorm2d(intOutput),
+                torch.nn.ReLU(inplace=False)
+            )
+
+        def Upsample2(nc, intOutput):
+            return torch.nn.Sequential(
+                torch.nn.ConvTranspose2d(in_channels=nc, out_channels=intOutput, kernel_size=4, stride=4, padding=1,
+                                         output_padding=2),
+                torch.nn.BatchNorm2d(intOutput),
+                torch.nn.ReLU(inplace=False)
+            )
+
+        self.decoder = nn.Sequential(
+            Basic(256, 128),
+            Upsample(128, 128),
+            Basic(128, 64),
+            Upsample(64, 64),
+            Upsample2(64, 64),
+            Gen(64, 3, 32),
+        )
+
+        self.de_dense = nn.Sequential(
+            nn.Linear(768, 256 * 16 * 16),
+            nn.ELU(),
+        )
+
+    def forward(self, x):
+        output = self.de_dense(x)
+        output = output.view(output.size(0), 256, 16, 16)
+        output = self.decoder(output)
+        return output
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels=3, norm=None):
+        super(Discriminator, self).__init__()
+
+        def discriminator_block(in_filters, out_filters, norm=None):
+            """Returns downsampling layers of each discriminator block"""
+            layers = [nn.Conv2d(in_filters, out_filters, 4, stride=2, padding=1)]
+            if norm == 'instance':
+                layers.append(nn.InstanceNorm2d(out_filters))
+            if norm == 'batch':
+                layers.append(nn.BatchNorm2d(out_filters))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.model = nn.Sequential(
+            *discriminator_block(in_channels * 2, 64, norm),
+            *discriminator_block(64, 128, norm),
+            *discriminator_block(128, 256, norm),
+            *discriminator_block(256, 512, norm),
+            nn.ZeroPad2d((1, 0, 1, 0)),
+            nn.Conv2d(512, 1, 4, padding=1, bias=False)
+        )
+
+    def forward(self, img_A, img_B):
+        # Concatenate image and condition image by channels to produce input
+        img_input = torch.cat((img_A, img_B), 1)
+        return self.model(img_input)
+
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity, self).__init__()
+        
+    def forward(self, x):
+        return x
+
+class VisionTransformer(nn.Module):
+    """ Vision Transformer """
+    def __init__(self,
+                 image_size=(256, 256),
+                 patch_size=(16, 16),
+                 emb_dim=768,
+                 mlp_dim=3072,
+                 num_heads=12,
+                 num_layers=12,
+                 num_classes=1000,
+                 attn_dropout_rate=0.0,
+                 dropout_rate=0.1,
+                 num_frames=4,
+                 nu=0.01,
+                 feat_dim=None):
+        super(VisionTransformer, self).__init__()
+        # class token
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, emb_dim))
+
+        self.spatial_transformer = timm.create_model('vit_base_patch32_384', num_classes=0, pretrained=False)
+        self.temporal_transformer = Encoder(
+            num_patches=num_frames,
+            emb_dim=emb_dim,
+            mlp_dim=mlp_dim,
+            num_layers=num_layers,
+            num_heads=num_heads,
+            dropout_rate=dropout_rate,
+            attn_dropout_rate=attn_dropout_rate)
+
+        # Decoder
+        def Upsample(nc, intOutput):
+            return torch.nn.Sequential(
+                torch.nn.ConvTranspose2d(in_channels=nc, out_channels=intOutput, kernel_size=3, stride=2, padding=1,
+                                         output_padding=1),
+                torch.nn.BatchNorm2d(intOutput),
+                torch.nn.ReLU(inplace=False)
+            )
+
+        self.decoder = Decoder()
+        # Sphere
+        self.nu = nu
+        self.radius = torch.nn.Parameter(torch.tensor([0.0]))
+        self.center = torch.nn.Parameter(torch.randn(emb_dim))
+        # self.cross_att = CrossAttention(768, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.)
+
+    def forward(self, x): # x: (b, n, c, h, w)
+        x = x.float().cuda()
+        for i in range(x.shape[1]):
+            spatial_cls_token = self.spatial_transformer(x[:, i]).unsqueeze(1)
+            if i == 0:
+                all_spatial_cls_token = spatial_cls_token
+            else:
+                all_spatial_cls_token = torch.cat((spatial_cls_token, all_spatial_cls_token), 1)
+
+        b, n, c = all_spatial_cls_token.shape
+        # prepend class token
+        cls_token = self.cls_token.repeat(b, 1, 1)
+        emb = torch.cat([cls_token, all_spatial_cls_token], dim=1)
+        emb = self.temporal_transformer(emb)
+        emb = emb[:,0,:].unsqueeze(1)
+        output = self.decoder(emb)
+        return output
+
+if __name__ == '__main__':
+    model = VisionTransformer(num_layers=2)
+    x = torch.randn((2, 3, 256, 256))
+    out = model(x)
+
+    state_dict = model.state_dict()
+
+    for key, value in state_dict.items():
+        print("{}: {}".format(key, value.shape))
